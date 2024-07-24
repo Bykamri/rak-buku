4789422d169c546dd3caf6cdf3555225ee7f40c1
Bykamri <mafrizalsuhartono@gmail.com>
2024-07-24 22:38:01 +0700

    tes golang ubuntu-wsl

--- backend/main.go
+++ backend/main.go
@@ -4,4 +4,18 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello Worlds!")	
+	
+    limit := 1000
+    oddNumbers := generateOddNumbers(limit)
+
+    fmt.Println(oddNumbers)	
+
+}
+
+func generateOddNumbers(limit int) []int {
+    var odds []int
+    for i := 1; i <= limit; i += 2 {
+        odds = append(odds, i)
+    }
+    return odds
 }
